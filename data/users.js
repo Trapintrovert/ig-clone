04f8e93e6eba2274f1aaed4d9e9e64ba5d7a67c8
Adedamola Adeniyi <mcneo062@gmail.com>
2022-04-03 11:30:42 +0100

    Stories completed

---
+++ data/users.js
@@ -0,0 +1,22 @@
+export const USERS = [
+    {
+        user: 'cleverqazi',
+        image: 'https://q.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'theqazman',
+        image: 'https://q.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'sugashaw',
+        image: 'https://q.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'sugashaw 778',
+        image: 'https://q.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'sugashaw 88',
+        image: 'https://q.imgur.com/dxjd7FB.jpg',
+    }
+]
